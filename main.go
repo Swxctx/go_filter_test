0a32191460d4c49b1be8591e1c0f3ecc3b1d83e4
xiechuang <swxctxone@yeah.net>
2017-11-13 13:51:22 +0800

    通过Bloom与Contains共同过滤

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	filterWords := "广告,电脑,不会,大家,我们,在一起,最高级,最大,最小,全世界,全国,全部的,最小的,最好的,免费的,包好,全部免费,微爱,"
+	// filterWords := "微爱"
 	// title := "屠龙一刀999"
 	// title := "金利来袜子男士纯棉夏季商务男袜中筒袜黑短"
 	// title := "网易新闻 - 头条视频资讯阅读平台"
@@ -37,13 +38,15 @@ func main() {
 	log.Println("flag:", usestring.PoolFilterContainsAny(title, filterWords))
 	log.Println()
 	// indexAny
-	log.Println("indexAny")
-	log.Println("flag:", usestring.PoolFilterIndexAny(title, filterWords))
-	log.Println()
 
 	// rune
 	log.Println("rune range")
 	log.Println("flag:", userune.PoolFilterRune(title, filterWords))
+	log.Println()
+
+	//bloom and contains
+	log.Println("Bloom And Contains")
+	log.Println("flag:", usestring.FilterBloomAndContains(title, filterWords))
 
 }
 
---
+++ usestring/bloomAndContains.go
@@ -0,0 +1,38 @@
+package usestring
+
+import (
+	"go_filter_test/util"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/bloom"
+)
+
+func FilterBloomAndContains(title string, filterWords string) bool {
+	start_ts := time.Now().UnixNano()
+	//remove ,
+	strFilterWords := strings.Split(filterWords, ",")
+
+	//bloom filter
+	n := uint(1000)
+	filter := bloom.New(20*n, uint(len(strFilterWords))) // load of 20, 5 keys
+
+	//add filter
+	for i := 0; i < len(strFilterWords); i++ {
+		//bloom filter add
+		filter.Add([]byte(strFilterWords[i]))
+	}
+
+	titleTran := util.ConvertTitle2Slice(title)
+	for _, v := range titleTran {
+		//bloom
+		if filter.Test([]byte(v)) && strings.Contains(filterWords, v) {
+			log.Println("bloom标题不合法-非法词:", v)
+			end_ts := time.Now().UnixNano()
+			log.Println("耗时(ns):", end_ts-start_ts)
+			return true
+		}
+	}
+	return false
+}
