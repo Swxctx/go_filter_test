50eaac73f869035928b9babd215f82e9c6b5d9b7
xiechuang <swxctxone@yeah.net>
2017-11-13 13:53:05 +0800

    remove

--- main.go
+++ main.go
@@ -10,11 +10,7 @@ import (
 )
 
 func main() {
-	filterWords := "广告,电脑,不会,大家,我们,在一起,最高级,最大,最小,全世界,全国,全部的,最小的,最好的,免费的,包好,全部免费,微爱,"
-	// filterWords := "微爱"
-	// title := "屠龙一刀999"
-	// title := "金利来袜子男士纯棉夏季商务男袜中筒袜黑短"
-	// title := "网易新闻 - 头条视频资讯阅读平台"
+	filterWords := "广告,电脑,不会,大家,我们,在一起,最高级,最大,最小,全世界,全国,全部的,最小的,最好的,免费的,包好,全部免费,"
 	title := "网易新闻 - 头条视频资讯阅读平台免费的"
 	// log.Println("blandcuk")
 	// log.Println("flag:", blandcuk.PoolFilter(title))
